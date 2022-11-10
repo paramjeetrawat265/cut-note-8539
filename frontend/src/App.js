ed7ea7bb02d988c9abaee8d37612fc2b558253fc
Antara12345 <antaraburman888@gmail.com>
2022-11-11 00:42:19 +0530

    homee

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
