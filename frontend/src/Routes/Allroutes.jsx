ec7e609a40407b024349da9d1596c3fbd932b87b
paramjeetrawat265 <paramjeetrawat265@gmail.com>
2022-11-11 20:27:57 +0530

    backend and frontend

--- frontend/src/Routes/Allroutes.jsx
+++ frontend/src/Routes/Allroutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Cart from './Pages/Cart'
+import { Dashboard } from './Pages/Dashboard'
 
 import Homepage from './Pages/Homepage'
 import MensProduct from './Pages/MensProduct'
@@ -9,13 +10,13 @@ import Womenpage from './Pages/Womenpage'
 const Allroutes = () => {
   return (
     <div>
-        <Routes>
-            <Route path='/' element={<Homepage/>}/>
-            <Route path='/menspage' element={<MensProduct/>}/>
-            <Route path='/womenpage' element={<Womenpage/>}/>
-            <Route path='/cart' element={<Cart/>}/>
-        </Routes>
-
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/menspage' element={<MensProduct />} />
+        <Route path='/womenpage' element={<Womenpage />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
     </div>
   )
 }
